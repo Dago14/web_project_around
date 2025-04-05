e98d172733cdc141594081481ae133c35430e2b4
Dagoberto Najera <dagobertonajeraperez23@gmail.com>
2025-04-04 23:21:21 -0600

    Template

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,48 @@ let buttonedit = content.querySelector("#button-edit");
 let popup = content.querySelector(".popup");
 let buttonclose = content.querySelector("#button-close");
 let buttonsave = content.querySelector("#button-save");
+let elements = content.querySelector(".elements");
+let template = content.querySelector("#template");
+
+const datos = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+datos.forEach((dato) => {
+  let clon = template.content.cloneNode(true);
+  let paragraph = clon.querySelector(".elements__paragraph");
+  let image = clon.querySelector(".elements__image");
+  let alt = clon.querySelector(".elements__image");
+
+  paragraph.textContent = dato.name;
+  image.src = dato.link;
+  alt.alt = dato.name;
+
+  elements.appendChild(clon);
+});
 
 // Value
 
