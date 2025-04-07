c68fd3cbc0b5d23d4cf2d08d211fe856c76b8138
Dagoberto Najera <dagobertonajeraperez23@gmail.com>
2025-04-07 09:20:24 -0600

    terminado

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,17 @@ let buttonsave = content.querySelector("#button-save");
 let elements = content.querySelector(".elements");
 let template = content.querySelector("#template");
 
+//Botones del popup agregar
+let buttonadd = content.querySelector("#button-add");
+let popupadd = content.querySelector(".popup-add");
+let buttoncloseadd = content.querySelector("#button-add-close");
+
+//Logica para agg las nuevas cartas
+let buttonsaveadd = content.querySelector("#button-addinfo");
+
+let titlevalue = content.querySelector("#title");
+let urlvalue = content.querySelector("#url");
+
 const datos = [
   {
     name: "Valle de Yosemite",
@@ -43,7 +54,7 @@ datos.forEach((dato) => {
   image.src = dato.link;
   alt.alt = dato.name;
 
-  elements.appendChild(clon);
+  elements.appendChild(clon); // agrega a todos
 });
 
 // Value
@@ -54,6 +65,7 @@ let ValueInfo = content.querySelector("#info");
 let TextName = content.querySelector(".profile__name");
 let TextInfo = content.querySelector(".profile__hobbies");
 
+//eventos
 buttonedit.addEventListener("click", () => {
   ValueName.value = TextName.textContent;
   ValueInfo.value = TextInfo.textContent;
@@ -74,6 +86,33 @@ buttonsave.addEventListener("click", () => {
     popup.classList.add("popup_opened");
   } else {
     popup.classList.remove("popup__disabled");
+  }
+});
+
+buttonadd.addEventListener("click", () => {
+  popupadd.classList.remove("popup-add_opened");
+});
+
+buttoncloseadd.addEventListener("click", () => {
+  popupadd.classList.add("popup-add_opened");
+});
+
+buttonsaveadd.addEventListener("click", (evt) => {
+  evt.preventDefault(); // quita el actualizar
+
+  let clon = template.content.cloneNode(true);
+  let paragraph = clon.querySelector(".elements__paragraph");
+  let image = clon.querySelector(".elements__image");
+  let alt = clon.querySelector(".elements__image");
+
+  if (titlevalue.value !== "" && urlvalue.value !== "") {
+    paragraph.textContent = titlevalue.value;
+    image.src = urlvalue.value;
+    alt.alt = titlevalue.value;
+    popupadd.classList.add("popup-add_opened");
+    elements.prepend(clon); // agrega al principio de la lista
+    titlevalue.value = "";
+    urlvalue.value = "";
   }
 });
 
